61665c5bdad91b0eafdc3fdde84d7affe9b4c896
Henrik Nørgaard <menneskefernis@gmail.com>
2020-02-11 23:14:37 +0100

    Add remember_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  before_create :create_remember_digest
+
   has_secure_password
+
+  private
+    def self.new_token
+      SecureRandom.urlsafe_base64.to_s
+    end
+
+    def self.encrypt_token(token)
+      Digest::SHA1.hexdigest(token)
+    end
+
+    def create_remember_digest
+      self.remember_token = User.new_token
+      self.remember_digest = User.encrypt_token(remember_token)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_210538) do
+ActiveRecord::Schema.define(version: 2020_02_11_220213) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_02_11_210538) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "remember_digest"
   end
 
 end
